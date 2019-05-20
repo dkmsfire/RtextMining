fec4ac5e34c439bfae44ffdb59719c38ae4b554c
dkmsfire <leozxc2009@gmail.com>
2019-05-21 00:39:11 +0800

    png() and dev.off() for output image

--- txt.R
+++ txt.R
@@ -46,9 +46,11 @@ for(i in 1:6){
 }
 par(mfrow = c(2,1))
 for(i in 1:6){
+  png(filename = paste0("model", i, ".png"))
+  par(mfrow = c(2,1))
   plot(pr[[i]][[2]] ~ pr[[i]][[1]], main = "precision-recall plot", xlab = "Recall level", ylab = "Precision", xlim = c(0,1), type = "line")
   plot(doc[[i]][[2]] ~ doc[[i]][[1]], main = "precision at docs plot", xlab = "Docs number", ylab = "Precision", type = "line")
-  
+  dev.off()
 }
 
 library(ggplot2)
@@ -57,28 +59,33 @@ library(ggplot2)
 pr1 = rbind(pr[[1]], pr[[3]], pr[[5]])
 pr1$group = c(rep("okapi", 11), rep("Laplace", 11), rep("Jelinek-Mercer", 11))
 
+png(filename = "stemming pr.png")
 ggplot(data = pr1, aes(x = level, y = precision, color = group)) +
-  geom_line() + xlab("Recall") + ggtitle("Method with stemming precision-recall plot")
-
+  geom_line() + xlab("Recall") + ggtitle("Methods with stemming precision-recall plot") + labs(color = "Methods")
+dev.off()
 ## the precision at docs plot
 doc1 = rbind(doc[[1]], doc[[3]], doc[[5]])
 doc1$group = c(rep("okapi", 9), rep("Laplace", 9), rep("Jelinek-Mercer", 9))
 
+png(filename = "stemming doc.png")
 ggplot(data = doc1, aes(x = docs, y = precision, color = group)) +
-  geom_line() + xlab("Docs number") + ggtitle("Method with stemming precision at docs plot")
-
+  geom_line() + xlab("Docs number") + ggtitle("Methods with stemming precision at docs plot") + labs(color = "Methods")
+dev.off()
 
 ### model without stemming 
 ##the p-r plot
 pr2 = rbind(pr[[2]], pr[[4]], pr[[6]])
 pr2$group = c(rep("okapi", 11), rep("Laplace", 11), rep("Jelinek-Mercer", 11))
 
+png(filename = "no stemming pr.png")
 ggplot(data = pr2, aes(x = level, y = precision, color = group)) +
-  geom_line() + xlab("Recall") + ggtitle("Method without stemming precision-recall plot")
-
+  geom_line() + xlab("Recall") + ggtitle("Methods without stemming precision-recall plot") + labs(color = "Methods")
+dev.off()
 ## the precision at docs plot
 doc2 = rbind(doc[[2]], doc[[4]], doc[[6]])
 doc2$group = c(rep("okapi", 9), rep("Laplace", 9), rep("Jelinek-Mercer", 9))
 
+png(filename = "no stemming doc.png")
 ggplot(data = doc2, aes(x = docs, y = precision, color = group)) +
-  geom_line() + xlab("Docs number") + ggtitle("Method without stemming precision at docs plot")
+  geom_line() + xlab("Docs number") + ggtitle("Methods without stemming precision at docs plot") + labs(color = "Methods")
+dev.off()
