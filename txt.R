c60a542b351e622af90f455471f3d92798dc32da
dkmsfire <leozxc2009@gmail.com>
2019-05-20 19:01:46 +0800

    finish get the 2 number from recall-precision and precision at docs

--- txt.R
+++ txt.R
@@ -7,10 +7,28 @@ okapi2 = read.csv("okapi2.txt")
 tfidf1 = read.csv("tfidf1.txt")
 tfidf2 = read.csv("tfidf2.txt")
 model = list(jm1, jm2, okapi1, okapi2, tfidf1, tfidf1)
+names(model) = c("jm1", "jm2", "okapi1", "okapi2", "tfidf1", "tfidf2")
+save(model, file = "model.rda")
+
+load("model.rda")
 for(i in 1:6){
+  docs = NULL
+  precision = NULL
+  for(j in 20:29){
+    num = stri_extract_all_regex(model[[i]][j,1], "[0-9.]+")
+    docs[j - 19] = num[[1]][1]
+    precision[j - 19] = num[[1]][2]
+  }
+  assign(paste0(names(model)[[i]], "docs"), data.frame(docs, precision))
+}
+
+for(i in 1:6){
+  level = NULL
+  precision = NULL
   for(j in 6:16){
     num = stri_extract_all_regex(model[[i]][j,1], "[0-9.]+")
-    level[] = num[[1]][1]
-    precision[] = num[[1]][2]
+    level[j - 5] = num[[1]][1]
+    precision[j - 5] = num[[1]][2]
   }
+  assign(paste0(names(model)[[i]], "pr"), data.frame(level, precision))
 }
